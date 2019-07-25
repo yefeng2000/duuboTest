6c53e3566d98816cb4d34d52be3f69bae70cf151
yefeng2000 <yuwotongjian@qq.com>
2019-07-25 16:24:15 +0800

    swaggerAPI文档

--- src/main/java/com/example/demo/action/TestAction.java
+++ src/main/java/com/example/demo/action/TestAction.java
@@ -3,14 +3,19 @@ package com.example.demo.action;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.example.demo.util.IDCardValidServiceTest;
 import com.example.demo.util.RedisUtil;
-
+@Api(tags="用户管理相关接口")
 @RestController
 public class TestAction {
 	
@@ -21,22 +26,30 @@ public class TestAction {
 	private IDCardValidServiceTest idCardValidService;
 	
 	@GetMapping("/test")
+	@ApiOperation("根据id更新用户的接口")
 	public Map<Object,Object> test() throws Exception{
-		Map<Object,Object> map = new HashMap<>();
+		Map<Object,Object> map = new HashMap<>(2);
 		map.put("hello", "world");
 		map.put("你好", "地球");
 		Object m = redisUtil.hmget("map");
-		if(m== null)
+		if(m== null) {
 			;//redisUtil.hmset("map", map);
-		else {
+		} else {
 			Map<String,Object> mp = (Map<String,Object>)m;
-			if(mp.isEmpty())
+			if(mp.isEmpty()) {
 				;//redisUtil.hmset("map", map);
+			}
 			System.out.println(m);
 			
 		}
-		idCardValidService.test();
 		return map;
+	}
+
+	@GetMapping("/hello/{name}")
+	@ApiOperation("根据id查询用户的接口")
+	@ApiImplicitParam(name="name",value="用户name",defaultValue="99",required=true)
+	public String hello(@PathVariable("name") String name,/*@ApiParam(name = "say",value = "内容",required = false)*/ String say){
+		return "用户："+name+" =="+say;
 	}
 
 }
---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo.action"))
+                .paths(PathSelectors.any()).build()
+                .apiInfo(new ApiInfoBuilder()
+                        .title("SpringBoot整合Swagger")
+                        .description("SpringBoot整合Swagger，详细信息......")
+                        .version("9.0")
+                        .contact(new Contact("啊啊啊啊", "blog.csdn.net", "aaa@gmail.com"))
+                        .license("The Apache License")
+                        .licenseUrl("http://www.baidu.com")
+                        .build());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        super.addResourceHandlers(registry);
+    }
+}
