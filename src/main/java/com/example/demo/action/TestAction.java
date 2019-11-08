f4f03fe08c89796fbc1448bba742e573d5633297
yefeng2000 <yuwotongjian@qq.com>
2019-11-08 16:28:54 +0800

    使用JSON格式化对象属性，打印json字符串日志信息

--- src/main/java/com/example/demo/action/TestAction.java
+++ src/main/java/com/example/demo/action/TestAction.java
@@ -1,12 +1,18 @@
 package com.example.demo.action;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.example.demo.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +24,9 @@ import com.example.demo.util.RedisUtil;
 @Api(tags="用户管理相关接口")
 @RestController
 public class TestAction {
-	
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(TestAction.class);
+
 	@Autowired
     private RedisUtil redisUtil;
 	//@Reference(version="1.0.1")
@@ -49,6 +57,14 @@ public class TestAction {
 	@ApiOperation("根据id查询用户的接口")
 	@ApiImplicitParam(name="name",value="用户name",defaultValue="99",required=true)
 	public String hello(@PathVariable("name") String name,/*@ApiParam(name = "say",value = "内容",required = false)*/ String say){
+		User user = new User();
+		user.setCreateTime(new Date());
+		user.setLoginName("test");
+		user.setNickName("风雨兼程");
+		user.setLoginPwd("8888888888");
+		user.setType("1");
+		LOGGER.info("请求参数信息：{}",JSON.toJSONStringWithDateFormat(user,"yyyy-MM-dd HH:mm:ss",SerializerFeature.WriteDateUseDateFormat));
+
 		return "用户："+name+" =="+say;
 	}
 
---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,65 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    private String nickName;
+
+    private String loginName;
+
+    private String loginPwd;
+
+    private String type;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getLoginPwd() {
+        return loginPwd;
+    }
+
+    public void setLoginPwd(String loginPwd) {
+        this.loginPwd = loginPwd;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
